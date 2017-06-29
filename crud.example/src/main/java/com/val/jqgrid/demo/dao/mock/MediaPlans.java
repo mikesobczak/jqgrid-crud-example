b1aadfbd2cd8168db145e6591a106ae1ef6d0496
mikesobczak <msobczak@yahoo.com>
2017-06-29 15:03:47 -0400

    Simplifying Spring context config.  This eliminated duplicate Spring context from being loaded.

--- crud.example/src/main/java/com/val/jqgrid/demo/dao/mock/MediaPlans.java
+++ crud.example/src/main/java/com/val/jqgrid/demo/dao/mock/MediaPlans.java
@@ -12,24 +12,6 @@ public class MediaPlans {
 	
 	private static Logger LOG = Logger.getLogger(MediaPlans.class);
 	
-	/*
-	private Map<Integer, MediaPlan> list;
-	
-	public MediaPlans () {
-		LOG.debug("inside MediaPlans ctor");
-		list = new HashMap<Integer, MediaPlan>();
-	}
-
-	public Map<Integer, MediaPlan> getList() {
-		return list;
-	}
-
-	public void setList(Map<Integer, MediaPlan> list) {
-		this.list = list;
-	}
-	*/
-	
-	//private MediaPlans mediaPlans;
 	private Map<Integer, MediaPlan> list;
 	
 	public MediaPlans() {
